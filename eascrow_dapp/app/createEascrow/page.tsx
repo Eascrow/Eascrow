30c4547ca99b409623caea25ac7bda814c1a0751
flo <f.imbert4@gmail.com>
2024-10-31 17:14:12 +0100

    update createEascrow page and add openDetails page

--- eascrow_dapp/app/createEascrow/page.tsx
+++ eascrow_dapp/app/createEascrow/page.tsx
@@ -1,251 +1,200 @@
 'use client';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+
+// import { useFreighterWallet } from '@/app/hooks/useFreighterWallet';
 // import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from '@/components/ui/card';
-import { useFreighterWaller } from '@/app/hooks/useFreighterWallet';
-import {
-  addressToScVal,
-  callWithSignedXDR,
-  getContractXDR,
-  numberToi128,
-} from '@/lib/utils';
+//   addressToScVal,
+//   callWithSignedXDR,
+//   getContractXDR,
+//   numberToi128,
+// } from '@/lib/utils';
 import Card from '@/components/shared/Card';
 
-export default function SmartContractUI() {
-  const { publicKey, signXDR, connect, hasFreighter } = useFreighterWaller();
-  const [contractState, setContractState] = useState({
-    initialized: false,
-    funded: false,
-    balance: 0,
-    buyer: '',
-    seller: '',
-    token: '',
-    price: 0,
+interface FormData {
+  email: string;
+  service: string;
+  amount: number | null;
+  terms: string;
+}
+
+export default function CreateEscrow() {
+  const router = useRouter();
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    service: '',
+    amount: null,
+    terms: '',
   });
 
-  const [formData, setFormData] = useState({
-    sacAddress: 'CDUXCICCRTDFNN56U75E4L66CYMC5JZR77WZKTEKS5YMMNGZW3MVXDL3',
-    buyerAddress: '',
-    sellerAddress: '',
-    tokenAddress: 'CDLZFC3SYJYDZT7K67VZ75HPJVIEUVNIXF47ZG2FB2RMQQVU2HHGCYSC',
-    price: '',
-  });
+  const [errors, setErrors] = useState<{ [key in keyof FormData]?: string }>(
+    {}
+  );
 
-  const [logs, setLogs] = useState<string[]>([]);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: name === 'amount' ? parseFloat(value) : value, // Convert amount to number
+    }));
   };
 
-  const addLog = (message: string) => {
-    setLogs((prevLogs) => [
-      ...prevLogs,
-      `${new Date().toLocaleTimeString()}: ${message}`,
-    ]);
+  const validateEmail = (email: string): boolean => {
+    // Check if the email ends with [value].[value]
+    const emailPattern = /^[\w-.]+@([\w-]+\.)+[a-z]{2,}$/i;
+    return emailPattern.test(email);
   };
 
-  const handleInitialize = async () => {
-    try {
-      console.log('formData', formData);
-      const contractParams = [
-        addressToScVal(formData.buyerAddress),
-        addressToScVal(formData.sellerAddress),
-        addressToScVal(formData.tokenAddress),
-        numberToi128(Number(formData.price)),
-      ];
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newErrors: { [key in keyof FormData]?: string } = {};
 
-      console.log('contractParams', contractParams.length);
-
-      /**
-       * This contract call will send the Assets to the Ticket Sale Contract
-       */
-      const xdr = await getContractXDR(
-        formData.sacAddress,
-        'initialize',
-        formData.buyerAddress, // Contract's caller
-        contractParams //
-      );
-
-      const signedXDR = await signXDR(xdr);
-      console.log('signedXDR', signedXDR, signedXDR.signedTxXdr);
-      const txResult = await callWithSignedXDR(signedXDR.signedTxXdr);
-      console.log('txResult', txResult);
-    } catch (error) {
-      console.error(error);
+    // Validate fields
+    if (!formData.email) {
+      newErrors.email = 'Email is required.';
+    } else if (!validateEmail(formData.email)) {
+      newErrors.email = 'The email format is incorrect.';
     }
-  };
 
-  const handleReleaseFunds = async () => {
-    try {
-      const contractParams = [];
-
-      /**
-       * This contract call will send the Assets to the Ticket Sale Contract
-       */
-      const xdr = await getContractXDR(
-        formData.sacAddress,
-        'release_funds',
-        formData.buyerAddress, // Contract's caller
-        contractParams //
-      );
-
-      const signedXDR = await signXDR(xdr);
-      console.log('signedXDR', signedXDR, signedXDR.signedTxXdr);
-      const txResult = await callWithSignedXDR(signedXDR.signedTxXdr);
-      console.log('txResult', txResult);
-    } catch (error) {
-      console.error(error);
+    if (!formData.service) {
+      newErrors.service = 'Service is required.';
+    } else if (!isNaN(Number(formData.service))) {
+      newErrors.service = 'Service must be a string, not a number.';
     }
-  };
 
-  const handleFund = async () => {
+    if (!formData.amount || formData.amount <= 0 || formData.amount === null) {
+      newErrors.amount = 'The amount must be greater than zero.';
+    }
+
+    if (!formData.terms) {
+      newErrors.terms = 'Terms are required.';
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return; // Do not submit if there are errors
+    }
+
     try {
-      const contractParams = [
-        addressToScVal(formData.buyerAddress),
-        numberToi128(Number(formData.price)),
-      ];
-
-      /**
-       * This contract call will send the Assets to the Ticket Sale Contract
-       */
-      const xdr = await getContractXDR(
-        formData.sacAddress,
-        'fund',
-        formData.buyerAddress, // Contract's caller
-        contractParams //
-      );
-
-      const signedXDR = await signXDR(xdr);
-      console.log('signedXDR', signedXDR, signedXDR.signedTxXdr);
-      const txResult = await callWithSignedXDR(signedXDR.signedTxXdr);
-      console.log('txResult', txResult);
+      // Save data to localStorage on successful submission
+      localStorage.setItem('formData', JSON.stringify(formData));
+      // Redirect to "openDetails" page after saving data
+      router.push('/openDetails');
     } catch (error) {
       console.error(error);
+    } finally {
+      // Optionally reset the form data
+      setFormData({ email: '', service: '', amount: 0, terms: '' });
+      // Reset errors after successful submission
+      setErrors({});
     }
   };
 
   return (
     <div className="pt-[70px] pb-5 px-9">
       <Card>
-        <div className="mb-10 flex justify-between items-center">
+        <div className="m-8 flex justify-center">
           <div className="flex justify-center items-center space-x-2">
             <Image
               src="/icons/arrow-swapp-green.png"
-              alt="Eascrow website"
+              alt="Escrow website"
               width="36"
               height="36"
               priority
             />
             <h2 className="text-2xl text-white font-bold">Create an Escrow</h2>
           </div>
-          <div>
-            {!hasFreighter && (
-              <p className="text-red-500">Freighter Wallet not detected</p>
-            )}
-            {!publicKey ? (
-              <Button
-                onClick={connect}
-                className="w-full bg-mintGreen text-background text-sm font-bold"
-              >
-                Connect Wallet
-              </Button>
-            ) : (
-              <p className=" text-mintGreen">Wallet connected: {publicKey}</p>
-            )}
-          </div>
         </div>
-        <div className="p-5">
-          <div className="space-y-4 flex flex-col items-center">
-            <div className="space-y-2 min-w-[40%]">
-              <Label htmlFor="sacAddress">Smart Contract Address</Label>
+        <div className=" flex flex-col items-center">
+          <form onSubmit={handleSubmit} className="w-[788px]">
+            <div className="mb-8">
+              <Label htmlFor="email" className="text-white text-sm">
+                Email address
+              </Label>
               <Input
-                value={formData.sacAddress}
-                className="bg-transparent"
-                id="sacAddress"
-                name="sacAddress"
-                onChange={handleInputChange}
+                placeholder="uihutofficial@gmail.com"
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="mt-2.5 py-[22px] px-[14px] border border-[#2c303d]"
               />
+              {errors.email && (
+                <span style={{ color: 'red' }}>{errors.email}</span>
+              )}
             </div>
-            <div className="space-y-2 min-w-[40%]">
-              <Label htmlFor="buyerAddress">Buyer Address</Label>
-              <Input
-                className="bg-transparent"
-                id="buyerAddress"
-                name="buyerAddress"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2 min-w-[40%]">
-              <Label htmlFor="sellerAddress">Seller Address</Label>
-              <Input
-                className="bg-transparent"
-                id="sellerAddress"
-                name="sellerAddress"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2 min-w-[40%]">
-              <Label htmlFor="tokenAddress">Token Address</Label>
-              <Input
-                value={formData.tokenAddress}
-                className="bg-transparent"
-                id="tokenAddress"
-                name="tokenAddress"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2 min-w-[40%]">
-              <Label htmlFor="price">Price</Label>
-              <Input
-                className="bg-transparent"
-                id="price"
-                name="price"
-                type="number"
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="min-w-[40%] flex justify-between">
-              <div className="space-x-2">
-                <span>1</span>
-                <Button
-                  onClick={handleInitialize}
-                  disabled={contractState.initialized}
-                  className="bg-background border border-mintGreen text-mintGreen text-sm font-bold"
-                >
-                  Initialize Contract
-                </Button>
+            <div className="mb-8 flex justify-between">
+              <div>
+                <Label htmlFor="service" className="text-white">
+                  Product or Service description
+                </Label>
+                <Input
+                  placeholder="Product/Service"
+                  type="text"
+                  id="service"
+                  name="service"
+                  value={formData.service}
+                  onChange={handleChange}
+                  required
+                  className="mt-2.5 w-[376px] py-[22px] px-[14px] border border-[#2c303d]"
+                />
+                {errors.service && (
+                  <span style={{ color: 'red' }}>{errors.service}</span>
+                )}
               </div>
-              <div className="space-x-2">
-                <span>2</span>
-                <Button
-                  onClick={handleFund}
-                  className="bg-background border border-mintGreen text-mintGreen text-sm font-bold"
-                >
-                  Fund
-                </Button>
-              </div>
-              <div className="space-x-2">
-                <span>3</span>
-                <Button
-                  onClick={handleReleaseFunds}
-                  className="bg-background border border-mintGreen text-mintGreen text-sm font-bold"
-                >
-                  Release Funds
-                </Button>
+              <div>
+                <Label htmlFor="amount" className="text-white">
+                  Amount
+                </Label>
+                <Input
+                  placeholder="0"
+                  type="number"
+                  id="amount"
+                  name="amount"
+                  onChange={handleChange}
+                  required
+                  className="mt-2.5 w-[376px] py-[22px] px-[14px] border border-[#2c303d]"
+                />
+                {errors.amount && (
+                  <span style={{ color: 'red' }}>{errors.amount}</span>
+                )}
               </div>
             </div>
-          </div>
+            <div className="mb-8">
+              <Label htmlFor="terms" className="text-white">
+                Terms
+              </Label>
+              <Input
+                placeholder="Terms"
+                type="text"
+                id="terms"
+                name="terms"
+                value={formData.terms}
+                onChange={handleChange}
+                required
+                className="mt-2.5 py-[22px] px-[14px] border border-[#2c303d]"
+              />
+              {errors.terms && (
+                <span style={{ color: 'red' }}>{errors.terms}</span>
+              )}
+            </div>
+            <div className="flex justify-center">
+              <Button
+                type="submit"
+                className="mt-2.5 w-[182px] py-[6px] px-[12px] bg-mintGreen text-background text-sm font-bold"
+              >
+                Create
+              </Button>
+            </div>
+          </form>
         </div>
       </Card>
     </div>
