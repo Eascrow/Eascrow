2c3c8284fc9d24202f41feaef5a65bcd6af93051
flo <f.imbert4@gmail.com>
2025-05-30 18:29:07 +0200

    test

--- eascrow_dapp/app/transactions/page.tsx
+++ eascrow_dapp/app/transactions/page.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Card from '@/components/shared/Card';
-// import { Button } from '@/components/ui/button';
 import { useFreighterWallet } from '@/app/hooks/useFreighterWallet';
 import TableComponent from '@/components/shared/TableComponent';
 
